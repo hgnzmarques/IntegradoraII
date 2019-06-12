d52ba4f9790b82a070b798b9a430762b0ecbd476
nei.neto <nei.neto@teialabs.com>
2019-06-12 19:40:59 -0300

    Separate SQL queries from python files

--- python-api/dbConnect.py
+++ python-api/dbConnect.py
@@ -30,13 +30,7 @@ def run_query(query_string: str):
 
 
 def get_vakinhas_and_their_accumulated_money():
-    query = (
-        "select id, title, valor from vakinhas v "
-        "left join(select sum(value)valor,vakinha_id from contributions "
-        "group by vakinha_id) as c on c.vakinha_id = v.id "
-        "where cast(ending_date as date) >= cast(now() as date) "
-        "order by valor asc limit(4)"
-    )
+    query = open("./queries/accumulated_vakinhas_money.sql").read()
     cursor = run_query(query)
     vakinhas = [Vakinha(*row) for row in cursor]
     return vakinhas
@@ -48,18 +42,18 @@ if __name__ == "__main__":
 
 
 
-#Select payment methods and the aproval %
-#select payment_method from contributions limit 10;
+# Select payment methods and the aproval %
+# select payment_method from contributions limit 10;
 
-#Seleciona todos os valores das vakinhas por purpose(tipo de vakinha), e pode ser usado para fazer o gráfico da mediana dos valores
-#SELECT ct.value as valor from contributions ct INNER JOIN vakinhas vk ON ct.vakinha_id = vk.id WHERE vk.purpose = (SELECT purpose FROM vakinhas WHERE id = "id da vakinha selecionada") limit 10
+# Seleciona todos os valores das vakinhas por purpose(tipo de vakinha), e pode ser usado para fazer o gráfico da mediana dos valores
+# SELECT ct.value as valor from contributions ct INNER JOIN vakinhas vk ON ct.vakinha_id = vk.id WHERE vk.purpose = (SELECT purpose FROM vakinhas WHERE id = "id da vakinha selecionada") limit 10
 
-#Seleciona uma Tupla sendo (count de quanto aparece, nome do tipo), procurando quais as "redes sociais" que foram usadas para informar a vakinha
-#select count(tipo)cnt, case when tipo is null then 'TOTAL' else tipo end TIPO from( SELECT case when utm_campaign in('whatsapp','facebook','twitter','mail') then utm_campaign else 'Outros' end tipo FROM contributions WHERE utm_campaign is not null)x GROUP BY rollup(x.tipo) ORDER BY cnt DESC
+# Seleciona uma Tupla sendo (count de quanto aparece, nome do tipo), procurando quais as "redes sociais" que foram usadas para informar a vakinha
+# select count(tipo)cnt, case when tipo is null then 'TOTAL' else tipo end TIPO from( SELECT case when utm_campaign in('whatsapp','facebook','twitter','mail') then utm_campaign else 'Outros' end tipo FROM contributions WHERE utm_campaign is not null)x GROUP BY rollup(x.tipo) ORDER BY cnt DESC
 
-#
-#
-#select id, title, valor from vakinhas v
-#outer apply(select sum(value) valor from contributions c where c.vakinha_id = v.id)c
+
+
+# select id, title, valor from vakinhas v
+# outer apply(select sum(value) valor from contributions c where c.vakinha_id = v.id)c
 # where cast(ending_date as date) >= cast(getdate() as date) order by valor asc limit(4)
 
